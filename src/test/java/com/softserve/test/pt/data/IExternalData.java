0e71e4feb81ff5fe6bc9c5406a275e1edfa355b7
Rostyslav Dembitskyi <rdem@softserveinc.com>
2016-02-12 17:22:57 +0200

    clean unused

--- src/test/java/com/softserve/test/pt/data/IExternalData.java
+++
@@ -1,9 +0,0 @@
-package com.softserve.test.pt.data;
-
-import java.util.List;
-
-public interface IExternalData {
-
-    List<List<String>> getAllCells(String absoluteFilePath);
-
-}
--- src/test/java/com/softserve/test/pt/data/IUrls.java
+++
@@ -1,9 +0,0 @@
-package com.softserve.test.pt.data;
-
-public interface IUrls {
-
-    String getLogin();
-
-    String getLogout();
-
-}
